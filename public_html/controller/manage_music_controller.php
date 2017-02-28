7260e530e6d47fd8e0fb43b2f24e266919726aee
Jan <jan@Luna.home>
2017-03-01 00:16:08 +0100

    Syntax changes

--- public_html/controller/manage_music_controller.php
+++ public_html/controller/manage_music_controller.php
@@ -2,65 +2,65 @@
 
 include_once 'PDO.php';
 
-  function addMusic($name, $type, $embed_link) {
-    $query = "INSERT INTO music(name, type, embed_link) VALUES (:name, :type, :embed_link);";
-    $statement = connect()->prepare($query);
+function addMusic($name, $type, $embed_link) {
+  $query = "INSERT INTO music(name, type, embed_link) VALUES (:name, :type, :embed_link);";
+  $statement = connect()->prepare($query);
 
-    try {
-        $statement->execute(['name' => $name, 'type' => $type, 'embed_link' => $embed_link]);
-        header("Location: ../manage_music.php");
-    }
-    catch(Exception $e) {
-        echo "manage_music_controller.php error in ---> addMusic(). Error message + " . $e->getMessage();
-    }
+  try {
+      $statement->execute(['name' => $name, 'type' => $type, 'embed_link' => $embed_link]);
+      header("Location: ../manage_music.php");
   }
-
-
-  function getAllMusic() {
-    $query = "SELECT * FROM music;";
-    $statement = connect()->prepare($query);
-
-    try {
-      $statement->execute();
-      $music = $statement->fetchAll(PDO::FETCH_CLASS);
-
-      return $music;
-    }
-    catch(Exception $e) {
-      echo "manage_music_controller.php error in ---> getAllMusic(). Error message + " . $e->getMessage();
-    }
+  catch(Exception $e) {
+      echo "manage_music_controller.php error in ---> addMusic(). Error message + " . $e->getMessage();
   }
+}
 
 
-  function getAllTracks() {
-    $query = "SELECT * FROM music WHERE type = 'track';";
-    $statement = connect()->prepare($query);
+function getAllMusic() {
+  $query = "SELECT * FROM music;";
+  $statement = connect()->prepare($query);
 
-    try {
-      $statement->execute();
-      $tracks = $statement->fetchAll(PDO::FETCH_CLASS);
+  try {
+    $statement->execute();
+    $music = $statement->fetchAll(PDO::FETCH_CLASS);
 
-      return $tracks;
-    }
-    catch(Exception $e) {
-      echo "manage_music_controller.php error in ---> getAllTracks(). Error message + " . $e->getMessage();
-    }
+    return $music;
   }
-
-
-  function getAllSets() {
-    $query = "SELECT * FROM music WHERE type = 'set';";
-    $statement = connect()->prepare($query);
-
-    try {
-      $statement->execute();
-      $sets = $statement->fetchAll(PDO::FETCH_CLASS);
-
-      return $sets;
-    }
-    catch(Exception $e) {
-      echo "manage_music_controller.php error in ---> getAllSets(). Error message + " . $e->getMessage();
-    }
+  catch(Exception $e) {
+    echo "manage_music_controller.php error in ---> getAllMusic(). Error message + " . $e->getMessage();
   }
+}
+
+
+function getAllTracks() {
+  $query = "SELECT * FROM music WHERE type = 'track';";
+  $statement = connect()->prepare($query);
+
+  try {
+    $statement->execute();
+    $tracks = $statement->fetchAll(PDO::FETCH_CLASS);
+
+    return $tracks;
+  }
+  catch(Exception $e) {
+    echo "manage_music_controller.php error in ---> getAllTracks(). Error message + " . $e->getMessage();
+  }
+}
+
+
+function getAllSets() {
+  $query = "SELECT * FROM music WHERE type = 'set';";
+  $statement = connect()->prepare($query);
+
+  try {
+    $statement->execute();
+    $sets = $statement->fetchAll(PDO::FETCH_CLASS);
+
+    return $sets;
+  }
+  catch(Exception $e) {
+    echo "manage_music_controller.php error in ---> getAllSets(). Error message + " . $e->getMessage();
+  }
+}
 
 ?>
