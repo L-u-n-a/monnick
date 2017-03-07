f338f3d9c876b39b4d9803df83ac287c844a8537
Jan <jan@Luna.home>
2017-03-07 18:33:59 +0100

    changd way contact information is displayed with php on the contact.php page.

--- public_html/contact.php
+++ public_html/contact.php
@@ -15,11 +15,9 @@ include_once "layout/navbar.php";
     <div class="col-xs-12 col-sm-12 col-md-12 track">
         <h1 style="font-size: 2.2em; text-align: center; line-height: 2em;">About</h1><br />
         <p style="line-height: 2em; letter-spacing: 0.05em; word-spacing: 0.2em; font-size: 1.1em;">
-          <?php
-            $text = getContactText();
 
-            echo $text->text;
-          ?>
+          <?php echo getContactText()->text; ?>
+
         </p>
     </div>
 </div>
