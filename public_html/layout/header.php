c00caf602391d05d9f401706fdb18fc8605ac729
luna <j.vannatijne@gmail.com>
2017-06-19 14:45:43 +0200

    paper audio player test

--- public_html/layout/header.php
+++ public_html/layout/header.php
@@ -2,7 +2,10 @@
 <html>
 <head>
     <meta name=viewport content="width=device-width, initial-scale=1">
+    <script src="bower_components/webcomponentsjs/webcomponents-lite.js"></script>
+    <link rel="import" href="../bower_components/paper-audio-player/paper-audio-player.html">
 
     <title>Index</title>
 </head>
 <body>
+<paper-audio-player src="track.mp3"></paper-audio-player>
